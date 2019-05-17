8e2a9aa42bb051fa2834dce05bdfb9d70f6fa917
jerry1101 <jerry110160@gmail.com>
2019-05-17 14:38:24 -0400

    find log files with converting exception

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,38 +1,17 @@
-## collecting
-#1. list file name+path with regex file name
-#2. read lines
-#3. storage
-import os
 from os import walk
+import os
 import pprint
+from glob import glob
 
-file_list = []
-dir_list=[]
 pp = pprint.PrettyPrinter(indent=4)
-PATH = 'C:\\test\\angular\\remote\\springboot_angular\\demo\\src\\test'
+result = []
 
-for root, dirs, files in walk(PATH):
-    for file in files:
-        file_list.append(os.path.join(root, file))
-    # file_list.extend(files)
-    # dir_list.extend(dirs)
-for file in files:
+PATH = '\\\\cs-p-log1\\ProductionLogging\\2019-04\\Exceptions\\Services'
+for x in walk(PATH):
+    for y in glob(os.path.join(x[0], '*.txt')):
+        result.append(y)
+pp.pprint(result)
 
-    pp.pprint(file)
-pp.pprint(file_list)
-pp.pprint(dir_list)
-
-########################
-def file_name_filter(path=""):
-   
-    if( os.path.basename(path).find('Excep') == -1):
-        return False
-    else:
-        return True
-
-
-
-## reformat
-
-
-## output
+for f in result:
+    if 'converting' in open(f).read():
+        pp.pprint(f)
