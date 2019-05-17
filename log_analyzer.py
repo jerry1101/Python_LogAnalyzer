84a6acd898eba9cb22366af404885d5eb79105c9
jerry <jerry110160@gmail.com>
2019-05-16 21:30:18 -0400

    list files

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,6 +2,27 @@
 #1. list file name+path with regex file name
 #2. read lines
 #3. storage
+import os
+from os import walk
+import pprint
+
+file_list = []
+dir_list=[]
+pp = pprint.PrettyPrinter(indent=4)
+PATH = 'C:\\test\\angular\\remote\\springboot_angular\\demo\\src\\test'
+
+for root, dirs, files in walk(PATH):
+    for file in files:
+        file_list.append(os.path.join(root, file))
+    # file_list.extend(files)
+    # dir_list.extend(dirs)
+for file in files:
+
+    pp.pprint(file)
+pp.pprint(file_list)
+pp.pprint(dir_list)
+
+
 
 ## reformat
 
